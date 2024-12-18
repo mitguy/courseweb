3845842eccc61185afe857e9f07c928e7a4c1c55
mitguy <164416260+mitguy@users.noreply.github.com>
2024-12-18 05:49:22 +0300

    use double quotes

--- src/components/UsernameBox.jsx
+++ src/components/UsernameBox.jsx
@@ -1,6 +1,6 @@
 export default function UsernameBox({ name }) {
   return (
-    <div className={`h-16 w-64 flex flex-row justify-center items-center bg-yellow`}>
+    <div className="h-16 w-64 flex flex-row justify-center items-center bg-yellow">
       <p className="text-2xl font-extrabold">{ name }</p>
     </div>
   );
